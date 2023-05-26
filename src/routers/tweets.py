409fff415a6640140888f2ea77fcff5782529502
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-05-26 17:57:21 +0300

    refactored

--- src/routers/tweets.py
+++ src/routers/tweets.py
@@ -3,99 +3,90 @@ This module contains routes for router `tweets`
 """
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Header
-from fastapi.requests import Request
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, update, delete
-
-from loguru import logger
 
 from database.init_db import db
-from database.models import Tweet, User, TweetLike
-from database.schemas import TweetIn, TweetOut
-from database.service import Dal
+from database.models import User
+from schemas.base_schema import BaseSchema
+from schemas.tweet_schema import TweetIn, TweetOut, TweetsOut
+from utils.authentication import get_current_user
+from utils.responses import RESPONSE_401_422_404, RESPONSE_401_422_404_403
+from utils.service import Dal
 
-router = APIRouter(prefix='/api/tweets', tags=['tweets'])
+router = APIRouter(prefix='/tweets', tags=['tweets'])
 
 
-@router.get('/')
-async def get_tweets(
-        api_key: Annotated[str | None, Header()] = None,
-        sess: AsyncSession = Depends(db)
+@router.get('/', response_model=TweetsOut, status_code=200)
+async def _get_tweets(
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
-    """Get all tweets from current user's followee."""
+    """Get all tweets from current user and user's followee."""
 
-    tweets = await Dal(sess).get_all_tweets()
-    logger.debug(f'{tweets=}')
-    return {"result": True, "tweets": tweets}
+    tweets = await Dal(sess).get_all_tweets(current_user)
+    return {"tweets": tweets}
 
 
 @router.post('/', response_model=TweetOut, status_code=201)
 async def add_tweet(
     tweet: TweetIn,
-    api_key: Annotated[str | None, Header()] = None,
-    sess: AsyncSession = Depends(db),
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
     """Post new tweet."""
-    logger.debug(f"{tweet=}")
-    logger.debug(f"{api_key=}")
 
-    tweet_data = tweet.dict()
-
-    curr_user = await Dal(sess).get_current_user(api_key)
-    tweet_data.update({'user_id': curr_user.id})
-
-    new_tweet = Tweet(**tweet_data)
-    sess.add(new_tweet)
-    await sess.commit()
-    # return await Dal(sess).add_one(Tweet, tweet)
-    return new_tweet
+    return await Dal(sess).add_tweet(tweet=tweet, user_id=current_user.id)
 
 
-@router.delete('/{idx}')
+@router.delete(
+    '/{idx}',
+    response_model=BaseSchema,
+    responses=RESPONSE_401_422_404_403,
+    status_code=200,
+)
 async def delete_tweet(
     idx: int,
-    api_key: Annotated[str | None, Header()] = None,
-    sess: AsyncSession = Depends(db),
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
-    """delete specific tweet"""
-    # if tweet.user == api-key user
-    pass
+    """Delete own specific tweet."""
+
+    await Dal(sess).delete_tweet(tweet_id=idx, user_id=current_user.id)
+    return {"result": True}
 
 
-@router.post('/{idx}/likes')
+@router.post(
+    '/{idx}/likes',
+    response_model=BaseSchema,
+    responses=RESPONSE_401_422_404,
+    status_code=201,
+)
 async def add_like_to_tweet(
     idx: int,
-    api_key: Annotated[str | None, Header()] = None,
-    sess: AsyncSession = Depends(db),
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
-    """add like to tweet"""
+    """Add like to tweet."""
 
-    stmt = select(User.id).filter_by(api_key=api_key)
-    async with sess.begin():
-        user_id = await sess.scalar(stmt)
-        tweet = await sess.get(Tweet, idx)
-        new_like = TweetLike(tweet_id=tweet.id, user_id=user_id)
-        sess.add(new_like)
-    # await sess.commit()
+    await Dal(sess).add_like_to_tweet(tweet_id=idx, user_id=current_user.id)
     return {'result': True}
 
 
-@router.delete('/{idx}/likes')
+@router.delete(
+    '/{idx}/likes',
+    response_model=BaseSchema,
+    responses=RESPONSE_401_422_404,
+    status_code=200,
+)
 async def remove_like_from_tweet(
     idx: int,
-    api_key: Annotated[str | None, Header()] = None,
-    sess: AsyncSession = Depends(db),
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
     """Remove like from tweet."""
 
-    curr_user = await Dal(sess).get_current_user(api_key)
-    async with sess.begin():
-        curr_tweet = await sess.get(Tweet, idx)
-
-    if curr_tweet.user_id == curr_user.id:
-        stmt = delete(TweetLike).filter_by(user_id=curr_user.id)
-        async with sess.begin():
-            await sess.execute(stmt)
-
+    await Dal(sess).remove_like_from_tweet(
+        tweet_id=idx, user_id=current_user.id
+    )
     return {'result': True}
--- src/routers/users.py
+++ src/routers/users.py
@@ -4,71 +4,90 @@ This module contains routes for router `tweets`
 
 from typing import Annotated
 
-from fastapi import APIRouter, Header, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, delete, and_
-
+from fastapi import APIRouter, Depends, HTTPException
 from loguru import logger
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
-from database.service import Dal
-from database.models import User, UserToUser
 from database.init_db import db
-from database.schemas import UserResponse
+from database.models import User
+from schemas.base_schema import BaseSchema
+from schemas.user_schema import UserOut
 
-router = APIRouter(prefix='/api/users', tags=['users'])
+# from database.schemas import UserOut
+from utils.authentication import get_current_user
+from utils.responses import RESPONSE_401, RESPONSE_401_422_404, RESPONSE_401_422_404_400
+from utils.service import Dal
 
-
-@router.get('/me')
-async def get_user(
-    sess: AsyncSession = Depends(db),
-    api_key: Annotated[str | None, Header()] = None,
-):
-    """Get current user account details."""
-    user = await Dal(sess).get_current_user(api_key)
-    return {"result": True, "user": user}
+router = APIRouter(prefix='/users', tags=['users'])
 
 
-@router.get('/{idx}')
+@router.get(
+    '/me',
+    # response_model=UserOut,
+    responses=RESPONSE_401,
+    status_code=200,
+)
+async def get_user(current_user: Annotated[User, Depends(get_current_user)]):
+    """Получить информацию о текущем пользователе"""
+    logger.debug(f'{current_user=}')
+    return {"user": current_user}
+
+
+@router.get(
+    '/{idx}',
+    response_model=UserOut,
+    responses=RESPONSE_401_422_404,
+    status_code=200,
+)
 async def get_user_by_id(idx: int, sess: AsyncSession = Depends(db)):
     """Get specific user details."""
-    user = await Dal(sess).get_user_by_idx(idx)
-    return {"result": True, "user": user}
+    user: User = await Dal(sess).get_user_by_idx(idx)
+    return {"user": user}
 
 
-@router.post('/{idx}/follow')
+@router.post(
+    '/{idx}/follow',
+    response_model=BaseSchema,
+    responses=RESPONSE_401_422_404_400,
+    status_code=200,
+)
 async def follow_user(
     idx: int,
-    sess: AsyncSession = Depends(db),
-    api_key: Annotated[str | None, Header()] = None,
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
     """follow specific user"""
-    current_user: User = await Dal(sess).get_current_user(api_key)
     user_for_follow: User = await Dal(sess).get_user_by_idx(idx)
-
-    async with sess.begin():
-        current_user.following = user_for_follow
+    if user_for_follow.id == current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Can't follow yourself",
+        )
+    if user_for_follow not in current_user.following:
+        async with sess.begin():
+            current_user.following.append(user_for_follow)
+            sess.add(current_user)
 
     return {'result': True}
 
 
-@router.delete('/{idx}/follow')
+@router.delete(
+    '/{idx}/follow',
+    response_model=BaseSchema,
+    responses=RESPONSE_401_422_404,
+    status_code=200,
+)
 async def unfollow_user(
     idx: int,
-    sess: AsyncSession = Depends(db),
-    api_key: Annotated[str | None, Header()] = None,
+    current_user: Annotated[User, Depends(get_current_user)],
+    sess: Annotated[AsyncSession, Depends(db)],
 ):
     """Unfollow specific user."""
-    current_user: User = await Dal(sess).get_current_user(api_key)
-    user_for_follow: User = await Dal(sess).get_user_by_idx(idx)
-
-    async with sess.begin():
-        # potential exc
-        stmt = delete(UserToUser).filter(
-            and_(
-                UserToUser.slave_id == current_user.id,
-                UserToUser.master_id == user_for_follow.id
-            )
-        )
-        await sess.execute(stmt)
+    user_for_unfollow: User = await Dal(sess).get_user_by_idx(idx)
+    if user_for_unfollow in current_user.following:
+        async with sess.begin():
+            current_user.following.remove(user_for_unfollow)
+            sess.add(current_user)
 
     return {'result': True}
