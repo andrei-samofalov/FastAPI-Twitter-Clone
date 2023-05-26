ddedad9cf42868d8da8c5d55ccc348722e36df70
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-05-27 02:03:03 +0300

    demo likes fix

--- src/database/migrations/versions/2023_05_26_1205-f4398eb0d2a4_add_users_data.py
+++ src/database/migrations/versions/2023_05_26_1205-f4398eb0d2a4_add_users_data.py
@@ -89,7 +89,7 @@ def upgrade() -> None:
         [
             {"tweet_id": 1, "user_id": 2},
             {"tweet_id": 2, "user_id": 1},
-            {"tweet_id": 3, "user_id": 1},
+            {"tweet_id": 3, "user_id": 2},
         ],
     )
 
